9a93327a2ce0e20be16e967752bb2c02949fee16
FineiasAntonio <fineias1187@gmail.com>
2024-01-23 01:13:56 -0300

    :sparkles: feat: adcionado camada de serviço para o project
    Discutir sobre o update com o front-end

--- src/main/java/com/squad31/apiorangeportifolio/Domain/Mapper/ProjectMapper.java
+++ src/main/java/com/squad31/apiorangeportifolio/Domain/Mapper/ProjectMapper.java
@@ -2,6 +2,7 @@ package com.squad31.apiorangeportifolio.Domain.Mapper;
 
 import com.squad31.apiorangeportifolio.Controller.Request.CreateProjectRequest;
 import com.squad31.apiorangeportifolio.Domain.Entity.Project;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -9,11 +10,18 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 @Component
+@Log4j2
 public class ProjectMapper {
 
-    public Project mapNewProject(CreateProjectRequest request) throws IOException {
+    public Project mapNewProject(CreateProjectRequest request) {
 
-        byte[] processedImage = request.image().getBytes();
+        byte[] processedImage = new byte[0];
+
+        try {
+            processedImage = request.image().getBytes();
+        } catch (IOException e) {
+            log.error("Erro ao processar imagem: {}", e.getMessage());
+        }
 
         return Project.builder()
                 .title(request.title())
--- src/main/java/com/squad31/apiorangeportifolio/Domain/Repository/UserRepository.java
+++ src/main/java/com/squad31/apiorangeportifolio/Domain/Repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.squad31.apiorangeportifolio.Domain.Repository;
 
 import com.squad31.apiorangeportifolio.Domain.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 }
