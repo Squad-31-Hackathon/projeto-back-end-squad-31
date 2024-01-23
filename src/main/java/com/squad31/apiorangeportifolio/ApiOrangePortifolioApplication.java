4fe4a7a635f2e1157f84722e110581e5bb79119c
Bruno <brunoandreotti@outlook.com>
2024-01-22 21:59:25 -0300

    :tada: feat: inicio do projeto

---
+++ src/main/java/com/squad31/apiorangeportifolio/ApiOrangePortifolioApplication.java
@@ -0,0 +1,13 @@
+package com.squad31.apiorangeportifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrangePortifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOrangePortifolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squad31/apiorangeportifolio/ApiOrangePortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad31.apiorangeportifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOrangePortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
