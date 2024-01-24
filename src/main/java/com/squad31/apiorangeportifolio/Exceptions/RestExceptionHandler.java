43fcca9eab09bf00b9c03b2ba94a08dc56881521
Bruno <brunoandreotti@outlook.com>
2024-01-24 17:31:12 -0300

    :sparkles: feat: criado regra para lidar com BadCredentialsException e AccessDeniedException

--- src/main/java/com/squad31/apiorangeportifolio/Exceptions/RestExceptionHandler.java
+++ src/main/java/com/squad31/apiorangeportifolio/Exceptions/RestExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.squad31.apiorangeportifolio.Exceptions;
 
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -33,4 +39,24 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleSecurityException(Exception ex) {
+
+        ProblemDetail errorDetail = null;
+
+        if (ex instanceof BadCredentialsException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), ex.getMessage());
+
+        }
+
+        if (ex instanceof AccessDeniedException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), ex.getMessage());
+
+        }
+
+        return errorDetail;
+    }
+
+
 }
