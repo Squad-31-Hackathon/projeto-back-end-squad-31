bf5e45a4bb13fd6733997e0363b823c5f9fb1b66
FineiasAntonio <fineias1187@gmail.com>
2024-01-25 14:25:16 -0300

    :heavy_plus_sign: build: adcionado a documentação da API com swagger

--- src/main/java/com/squad31/apiorangeportifolio/ApiOrangePortifolioApplication.java
+++ src/main/java/com/squad31/apiorangeportifolio/ApiOrangePortifolioApplication.java
@@ -3,7 +3,12 @@ package com.squad31.apiorangeportifolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.info.Info;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "api-squad-31", version = "1.0", description = "API Squad-31"))
 public class ApiOrangePortifolioApplication {
 
 	public static void main(String[] args) {
