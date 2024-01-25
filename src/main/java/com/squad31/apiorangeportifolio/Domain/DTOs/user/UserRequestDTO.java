a71999fc5fdcf6a8230cb99ef5ca9e7af2096bdd
Bruno <brunoandreotti@outlook.com>
2024-01-25 14:59:14 -0300

    :recycle: refactor: Alterado validações e campos entidade user

--- src/main/java/com/squad31/apiorangeportifolio/Domain/DTOs/user/UserRequestDTO.java
+++ src/main/java/com/squad31/apiorangeportifolio/Domain/DTOs/user/UserRequestDTO.java
@@ -1,21 +1,25 @@
 package com.squad31.apiorangeportifolio.Domain.DTOs.user;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 
-public record UserRequestDTO (
 
 
-        String name,
+public record UserRequestDTO (@NotBlank(message = "Usuário deve conter nome")
+                              String name,
 
-        String lastName,
+                              @NotBlank(message = "Usuário deve conter sobrenome")
+                              String lastName,
 
-        String email,
+                              @Email(message = "Usuário deve conter email válido")
+                              String email,
 
-        String password
-
-) {}
+                              @NotBlank(message = "Usuário deve conter senha")
+                              String password) {}
 
 
