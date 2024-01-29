cd58ed09273e0ef345ca5604b9bfa604d183044d
FineiasAntonio <fineias1187@gmail.com>
2024-01-29 02:14:52 -0300

    :sparkles: feat: armazenamento de imagens otimizado

--- src/main/java/com/squad31/apiorangeportifolio/Domain/Mapper/ProjectMapper.java
+++ src/main/java/com/squad31/apiorangeportifolio/Domain/Mapper/ProjectMapper.java
@@ -5,17 +5,16 @@ import com.squad31.apiorangeportifolio.Domain.DTOs.Project.ProjectResponseDTO;
 import com.squad31.apiorangeportifolio.Domain.Entity.Project;
 import com.squad31.apiorangeportifolio.Domain.Entity.User;
 import com.squad31.apiorangeportifolio.Domain.Repository.UserRepository;
+import com.squad31.apiorangeportifolio.Exceptions.InternalServerErrorException;
 import com.squad31.apiorangeportifolio.Exceptions.NotFoundException;
+import com.squad31.apiorangeportifolio.Utils.ImageUtils;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.Base64;
 import java.util.UUID;
 
 @Component
@@ -28,8 +27,7 @@ public class ProjectMapper {
     public Project mapNewProject(ProjectRequestDTO request, byte[] image) throws IOException {
 
         User user = userRepository.findById(UUID.fromString(request.userUuid()))
-                                  .orElseThrow(() -> new NotFoundException("Usuário não encontrado"));
-
+                .orElseThrow(() -> new NotFoundException("Usuário não encontrado"));
 
 
         return Project.builder()
@@ -39,24 +37,28 @@ public class ProjectMapper {
                 .description(request.description())
                 .link(request.link())
                 .publishDate(Date.valueOf(LocalDate.now()))
-                .image(image)
+                .image(ImageUtils.compressImage(image))
                 .build();
     }
 
     public static ProjectResponseDTO mapProjectResponse(Project project) {
 
-        String encodedImage = Base64.getEncoder().encodeToString(project.getImage());
-
-        return new ProjectResponseDTO(
-                project.getUuid().toString(),
-                project.getTitle(),
-                project.getTags(),
-                project.getDescription(),
-                project.getLink(),
-                project.getPublishDate(),
-                UserMapper.mapFromUserToUserResponse(project.getUser()),
-                project.getImage()
-        );
+        try {
+            return new ProjectResponseDTO(
+                    project.getUuid().toString(),
+                    project.getTitle(),
+                    project.getTags(),
+                    project.getDescription(),
+                    project.getLink(),
+                    project.getPublishDate(),
+                    UserMapper.mapFromUserToUserResponse(project.getUser()),
+                    ImageUtils.decompressImage(project.getImage())
+            );
+        } catch (Exception e) {
+            throw new InternalServerErrorException("Erro ao processar response"); // Essa exception só será disparada se houver erro ao processar imagem, se quiser tratar melhor pode substituir
+        }
     }
 
 }
+
+
--- src/main/java/com/squad31/apiorangeportifolio/Domain/Service/ProjectService.java
+++ src/main/java/com/squad31/apiorangeportifolio/Domain/Service/ProjectService.java
@@ -10,7 +10,6 @@ import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
